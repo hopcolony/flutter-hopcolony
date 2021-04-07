fd924d18fa4a37e77bb3ce6c0f427634ac508b19
Luis Paarup <lpaarup@gmail.com>
2021-04-07 18:46:16 +0200

    Changed namespace mapping to username

--- packages/hop_doc/example/lib/screens/home.dart
+++ packages/hop_doc/example/lib/screens/home.dart
@@ -10,6 +10,25 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   final instance = HopDoc.instance;
 
+  getIndices() async {
+    List<Index> resp = await instance.get();
+    resp.forEach((element) {
+      print(element.name);
+    });
+  }
+
+  getDocumentIds() async {
+    final snapshot = await instance
+        .index("pisos.com.properties")
+        .get(size: 10, onlyIds: true);
+    if (snapshot.success) {
+      snapshot.docs.forEach((doc) {
+        print(doc.id);
+        print(doc.source);
+      });
+    }
+  }
+
   add() async {
     final resp = await instance.index("profile").add({
       "name": Random().nextInt(255).toString(),
@@ -76,6 +95,39 @@ class _HomeScreenState extends State<HomeScreen> {
     }
   }
 
+  initState() {
+    getLogs();
+    super.initState();
+  }
+
+  List<Document> logs = [];
+  Future<void> getLogs() async {
+    final IndexSnapshot snapshot = await instance
+        .index(".hop.logs-rentai-job-pisos.com-16162625855760267--")
+        .limit(5)
+        .orderBy(field: "time", order: "asc")
+        .get();
+    if (snapshot.success) {
+      setState(() {
+        logs = snapshot.docs;
+      });
+    }
+  }
+
+  Future<void> moreLogs() async {
+    final IndexSnapshot snapshot = await instance
+        .index(".hop.logs-rentai-job-pisos.com-16162625855760267--")
+        .limit(5)
+        .orderBy(field: "time", order: "asc")
+        .start(after: logs.last, nanoDate: true)
+        .get();
+    if (snapshot.success) {
+      setState(() {
+        logs = snapshot.docs;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -84,27 +136,30 @@ class _HomeScreenState extends State<HomeScreen> {
       ),
       body: Column(
         children: [
-          StreamBuilder<DocumentSnapshot>(
-              stream: instance.index("profile").document("1234").stream(),
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  return Column(children: [
-                    if (snapshot.data.success)
-                      Text(snapshot.data.doc.source.toString()),
-                    TextButton(onPressed: update, child: Text("Update")),
-                    TextButton(onPressed: add, child: Text("Add")),
-                    TextButton(onPressed: delete, child: Text("Delete")),
-                    TextButton(onPressed: getAll, child: Text("Get all")),
-                    TextButton(
-                        onPressed: geoDistanceQuery,
-                        child: Text("Geo distance query")),
-                    TextButton(
-                        onPressed: geoBoxQuery, child: Text("Geo box query")),
-                  ]);
-                } else {
-                  return CircularProgressIndicator();
-                }
-              })
+          Container(
+            width: 400,
+            height: 400,
+            child: Scaffold(
+              body: Center(
+                child: Column(
+                  children: logs.map((e) => Text(e.source["log"])).toList(),
+                ),
+              ),
+              floatingActionButton: FloatingActionButton(
+                onPressed: moreLogs,
+                child: Text("More"),
+              ),
+            ),
+          ),
+          TextButton(onPressed: getIndices, child: Text("Get indices")),
+          TextButton(onPressed: getDocumentIds, child: Text("Get doc ids")),
+          TextButton(onPressed: update, child: Text("Update")),
+          TextButton(onPressed: add, child: Text("Add")),
+          TextButton(onPressed: delete, child: Text("Delete")),
+          TextButton(onPressed: getAll, child: Text("Get all")),
+          TextButton(
+              onPressed: geoDistanceQuery, child: Text("Geo distance query")),
+          TextButton(onPressed: geoBoxQuery, child: Text("Geo box query")),
         ],
       ),
     );
